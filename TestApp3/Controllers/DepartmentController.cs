4a9a45850a3508d9bbae1a4cc390107d91595c93
Surendra Kandira <surendrakandira@gmail.com>
2023-09-10 09:16:38 +0530

    Add project files.

---
+++ TestApp3/Controllers/DepartmentController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestApp3.Dto;
+using TestApp3.Models;
+
+namespace TestApp3.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+           
+      var  para=  db.Departments.Select(x => new DepartmentDto() {Id=x.Id,Name=x.Name,Code=x.Code}).ToList();
+            return View(para);
+        }
+        [HttpGet]
+        public IActionResult AddEdit(int ?id)
+        {
+            DepartmentDto dep= new DepartmentDto();
+
+            return View(dep);
+        }
+        [HttpPost]
+        public IActionResult AddEdit(DepartmentDto model)
+        {
+            Department department= new Department();
+            department.Id = model.Id;
+            department.Name = model.Name;
+            department.Code = model.Code;
+            
+            db.Departments.Add(department);
+            db.SaveChanges();
+
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            var para = db.Departments.Find(id);
+            db.Departments.Remove(para);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+
+    }
+    
+}
---
+++ TestApp3/Models/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestApp3.Models;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=SURENDRAKANDIRA\\SQLEXPRESS;Initial Catalog=Tastapp;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departme__3214EC07AD4D43B2");
+
+            entity.ToTable("Department");
+
+            entity.Property(e => e.Code)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
